b3ce9d4c6f2bdc5ef90559e2602dc5d3eb4dd301
Leman <leman@lmswork.com>
2018-02-07 23:22:08 +0800

    Update PlayerController.swift
    Change the class into "open"

--- Classes/Player/PlayerController.swift
+++ Classes/Player/PlayerController.swift
@@ -10,7 +10,7 @@
 
 import Foundation
 
-class PlayerController: NSObject, Player {
+open class PlayerController: NSObject, Player {
     
     /************************************************************/
     // MARK: - Properties
